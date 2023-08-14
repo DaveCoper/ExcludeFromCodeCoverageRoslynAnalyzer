7fc35114120032c59517caf13a98352d43334cd9
David Kubát <david.kubat@tietoevry.com>
2023-08-14 16:00:09 +0200

    Add project files.

---
+++ ExcludeFromCodeCoverageRoslynAnalyzer/IgnoreTestsSyntaxRewriter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ExcludeFromCodeCoverageRoslynAnalyzer
+{
+    public class IgnoreTestsSyntaxRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode? VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            if (node.BaseList != null &&
+                node.BaseList.Types.Any(tp => tp.Type?.ToString().StartsWith("DefaultDataGridConfigurationBase") == true &&
+                !HasAttribute(node, "ExcludeFromCodeCoverage")))
+            {
+                var newAttribute = SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage"));
+                var syntaxList = SyntaxFactory.SeparatedList<AttributeSyntax>();
+                syntaxList = syntaxList.Add(newAttribute);
+
+                var attributes = SyntaxFactory.AttributeList(syntaxList)
+                    .WithTrailingTrivia(SyntaxFactory.ParseTrailingTrivia("\n    "))
+                    .WithLeadingTrivia(SyntaxFactory.ParseLeadingTrivia("    "));
+
+                // save comments
+                var trivia = node.GetLeadingTrivia();
+
+                // remove comments
+                node = node.WithoutLeadingTrivia();
+
+                // add attribute lists
+                node = node.AddAttributeLists(attributes);
+
+                // restore comments to make them first
+                node = node.WithLeadingTrivia(trivia);
+            }
+
+            return node;
+        }
+
+        private bool HasAttribute(ClassDeclarationSyntax classDeclaration, string name)
+        {
+            return classDeclaration.AttributeLists.Any(list => list.Attributes.Any(attr => attr.Name.ToString().Contains(name, StringComparison.OrdinalIgnoreCase)));
+        }
+    }
+}
---
+++ ExcludeFromCodeCoverageRoslynAnalyzer/MySolutionAnalyzer.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ExcludeFromCodeCoverageRoslynAnalyzer
+{
+    public class MySolutionAnalyzer : BackgroundService
+    {
+        private readonly IHostApplicationLifetime applicationLifetime;
+        private readonly ILogger<MySolutionAnalyzer> logger;
+
+        public MySolutionAnalyzer(IHostApplicationLifetime applicationLifetime, ILogger<MySolutionAnalyzer> logger)
+        {
+            this.applicationLifetime = applicationLifetime;
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var cmdArgs = Environment.GetCommandLineArgs();
+
+            try
+            {
+                await Analyze(cmdArgs[1], stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Something broke");
+            }
+        }
+
+        private async Task Analyze(string slnPath, CancellationToken stoppingToken)
+        {
+            StringBuilder warnings = new StringBuilder();
+
+            logger.LogInformation("Looking for msbuild.");
+
+            MSBuildLocator.RegisterDefaults();
+            using (var workspace = MSBuildWorkspace.Create())
+            {
+                workspace.WorkspaceFailed += OnWorkspaceFailed;
+                workspace.WorkspaceChanged += OnWorkspaceChanged;
+
+                stoppingToken.ThrowIfCancellationRequested();
+                logger.LogInformation($"Loading solution {slnPath}.");
+
+                var currSolution = await workspace.OpenSolutionAsync(slnPath);
+                bool hasChanges = false;
+
+                foreach (var currProject in currSolution.Projects)
+                {
+                    stoppingToken.ThrowIfCancellationRequested();
+
+                    if (!currProject.Name.Contains("Quality", StringComparison.Ordinal))
+                    {
+                        logger.LogInformation($"Skipping project {currProject.Name}.");
+                        continue;
+                    }
+
+                    logger.LogInformation($"Processing project {currProject.Name}.");
+                    foreach (var document in currProject.Documents)
+                    {
+                        stoppingToken.ThrowIfCancellationRequested();
+                        var tree = await document.GetSyntaxTreeAsync();
+                        if (tree == null)
+                        {
+                            logger.LogError("Failed to open syntax tree for document {0}", document.Name);
+                            continue;
+                        }
+
+                        var root = tree.GetRoot();
+                        var rewriter = new IgnoreTestsSyntaxRewriter();
+                        root = rewriter.Visit(root);
+                        var newDocument = document.WithSyntaxRoot(root);
+
+                        currSolution = currSolution.WithDocumentSyntaxRoot(document.Id, root);
+                    }
+
+                    if (hasChanges)
+                    {
+                        logger.LogInformation("Applying changes to sollution {0}", currSolution.Id.Id);
+                        if (workspace.TryApplyChanges(currSolution))
+                        {
+                            logger.LogError("Failed to apply changes to solution {0}", currSolution.Id.Id);
+                        }
+                    }
+                    else
+                    {
+                        logger.LogInformation("Sollution {0} has no changes", currSolution.Id.Id);
+                    }
+                }
+            }
+
+            applicationLifetime.StopApplication();
+        }
+
+        private void OnWorkspaceChanged(object? sender, WorkspaceChangeEventArgs e)
+        {
+            logger.LogInformation(e.ToString());
+        }
+
+        private void OnWorkspaceFailed(object? sender, WorkspaceDiagnosticEventArgs e)
+        {
+            LogDiagnostic(e);
+        }
+
+        private void LogDiagnostic(WorkspaceDiagnosticEventArgs e)
+        {
+            switch (e.Diagnostic.Kind)
+            {
+                case WorkspaceDiagnosticKind.Warning:
+                    logger.LogWarning(e.Diagnostic.Message);
+                    break;
+
+                case WorkspaceDiagnosticKind.Failure:
+                    logger.LogError(e.Diagnostic.Message);
+                    break;
+            }
+        }
+    }
+}
